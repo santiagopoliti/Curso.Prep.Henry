bf072f9da57dbdb1c81d19a3ffcb8e559beff253
santiagopoliti <santiagopoliti@hotmail.com>
2021-02-25 16:41:09 -0300

    finish homework 2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -232,10 +232,11 @@ function esVocal(letra){
   //Verificar si el usuario ingresó un string de más de un carácter y, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   //Escribe tu código aquí
+  if(letra.length > 1){
+    return "Dato incorrecto"
+  }
   if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
     return "Es vocal"
-  } else{
-    return "Dato Incorrecto"
   }
 }
 
